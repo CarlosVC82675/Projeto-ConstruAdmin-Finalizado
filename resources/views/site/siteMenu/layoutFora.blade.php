4c27fbc4485d51aa16813383ea08c12ea3e7123d
CarlosVC82675 <carlosestudante82675@gmail.com>
2023-12-11 20:10:44 -0300

    Hospedagem/Tentativa7/aaaaaa

--- resources/views/site/siteMenu/layoutFora.blade.php
+++ resources/views/site/siteMenu/layoutFora.blade.php
@@ -11,7 +11,7 @@
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     {{--CSS--}}
-    <link rel="stylesheet" href="{{url("css/layoutFora.css")}}">
+    <link rel="stylesheet" href="{{asset('css/layoutFora.css')}}">
     {{--boostrap--}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     {{--Font awesome Icon--}}
@@ -161,7 +161,7 @@
 
         </div>
     </div>
-    <script src="{{url("js/layoutFora.js")}}"></script>
+    <script src="{{asset('js/layoutFora.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
